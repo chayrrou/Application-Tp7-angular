cc390a1678c15e067ce3d54242f1afce4c1274f7
chayrrou <chayrrou.mariem@gmail.com>
2022-11-02 11:48:49 -0400

    FormTp7

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddproduitComponent } from './application/addproduit/addproduit.component';
+import { ListproduitsComponent } from './application/listproduits/listproduits.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddproduitComponent,
+    ListproduitsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/application/listproduits/listproduits.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { VirtualTimeScheduler } from 'rxjs';
+import { Produit } from 'src/app/model/produit';
+import { ProduitService } from 'src/app/services/produit.service';
+
+@Component({
+  selector: 'app-listproduits',
+  templateUrl: './listproduits.component.html',
+  styleUrls: ['./listproduits.component.css']
+})
+export class ListproduitsComponent implements OnInit {
+
+  lesProduits: Produit[]=[]
+
+  constructor(private produitService:ProduitService) { }
+
+  ngOnInit(): void {
+    this.lesProduits = this.produitService.getProduits();
+  }
+
+}
